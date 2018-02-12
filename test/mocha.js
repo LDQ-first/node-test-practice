bc188f6edecae9f1addeaa69087b1a7b5991edc5
LDQ-first <178848890@qq.com>
2018-02-12 13:02:00 +0800

    1. add cover in math and modify mocha 2. add travis

--- test/mocha.js
+++ test/mocha.js
@@ -1,5 +1,5 @@
-const { add, mul } = require('../src/math')
-const { should, expect, assert} = require('chai')
+const { add, mul, cover } = require('../src/math')
+const { expect } = require('chai')
 
 describe('#math', () => {
   describe('add', () => {
@@ -12,13 +12,24 @@ describe('#math', () => {
     /* it.only('should return -5 when -2 + -3', () => {
       expect(add(-2, -3), -5)
     }) */
-    it.skip('should return 0 when 0 + -0', () => {
+     it.skip('should return 0 when 0 + -0', () => {
       expect(add(0, -0), 0)
     })
   })
-  describe('mul', () => {
+  describe('#mul', () => {
     it('should return 6 when 2 * 3', () => {
-      expect(add(2, 3), 6)
+      expect(mul(2, 3), 6)
+    })
+  })
+  describe('#cover', () => {
+    it('should return 1 when cover(2, 1)', () => {
+      expect(cover(2, 1)).to.equal(1)
+    })
+    it('should return 3 when cover(1, 2)', () => {
+      expect(cover(1, 2)).to.equal(3)
+    })
+    it('should return 4 when cover(2, 2)', () => {
+      expect(cover(2, 2)).to.equal(4)
     })
   })
 })
