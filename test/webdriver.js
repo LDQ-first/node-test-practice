c9ea6a23787ccefb12b0bb948624e607d4eafaa1
LDQ-first <178848890@qq.com>
2018-02-12 17:14:41 +0800

    add webdriver

---
+++ test/webdriver.js
@@ -0,0 +1,54 @@
+const { expect } = require('chai')
+const webdriver = require('selenium-webdriver')
+const chromeDriver = require('selenium-webdriver/chrome')
+const By = webdriver.By
+
+describe('百度首页 UI 测试', function () {
+  this.timeout(50000)
+
+  let driver
+
+  before(() => {
+    driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    // .setChromeOptions(new chromeDriver.Options().addArguments(['headless']))
+    .build()
+  })
+
+  it('should have title "百度一下，你就知道"', done => {
+    driver.get('https://www.baidu.com').then(() => {
+      driver.getTitle().then(title => {
+        expect(title).to.equal('百度一下，你就知道')
+        done()
+      })
+    }, (err) => {
+      console.log('err: ', err) 
+    })
+  })
+
+  it('shoule have button value 百度一下', done => {
+    driver.findElement(By.id('su')).then(button => {
+      button.getAttribute('value').then(val => {
+        expect(val).to.equal('百度一下')
+        done()
+      })
+    })
+  })
+
+  it('搜索 test', async () => {
+    const input = await driver.findElement(By.id('kw'))
+    await input.sendKeys('test')
+    driver.sleep(100000000)
+    const button = await driver.findElement(By.id('su'))
+    await button.click()
+    const handler = await driver.getWindowHandle()
+    await driver.switchTo().window(handler)
+    driver.sleep(1000)
+    const title = await driver.getTitle()
+    expect(title).to.equal('test_百度搜索')
+  })
+
+  after(() => {
+    driver.quit()
+  })
+})
